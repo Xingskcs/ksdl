83865c5766ff5306675fa6cb0ba7b13c8fe00f72
Xingskcs <csskxing@163.com>
2018-11-07 22:40:43 +0800

    gan scale policy

--- gan/gan_qos.py
+++ gan/gan_qos.py
@@ -12,6 +12,8 @@ scale_time = 0
 scale_delay = 8 # training begins after 8s when submit job.
 load_data_delay = 1.6 # take 1.6s to load training data to memory
 max_worker_number = 32
+ps_cpu = 2000
+ps_number = 1
 
 
 def submit_job():
@@ -34,6 +36,74 @@ def scale_worker():
     worker_number = worker_number*2
     os.system(change_worker_cmd)
     submit_job()
+
+
+def scale_ps_horizontally():
+    global ps_number
+    change_ps_cmd = "sed -i 's/{{%- set ps_replicas = {number1} -%}}/{{%- set ps_replicas = {number2} -%}}/g' distributed-gan.jinja".format(
+        number1=ps_number, number2=ps_number*2)
+    ps_number = ps_number*2
+    os.system(change_ps_cmd)
+
+
+def scale_ps_vertically():
+    """Scale ps."""
+    global ps_cpu
+    ps_cpu = ps_cpu+2000
+    # Change ps cpus
+    rm_cpu_request = "sed -i '63d' distributed-gan.jinja"
+    os.system(rm_cpu_request)
+    modify_cpu_request = """sed -i '62a \        cpu: "{cpu}m"' distributed-gan.jinja""".format(cpu=ps_cpu)
+    os.system(modify_cpu_request)
+    rm_cpu_limit = "sed -i '66d' distributed-gan.jinja"
+    os.system(rm_cpu_limit)
+    modify_cpu_limit = """sed -i '65a \        cpu: "{cpu}m"' distributed-gan.jinja""".format(cpu=ps_cpu)
+    os.system(modify_cpu_limit)
+
+
+def scale_ps_or_worker():
+    """Judge scale ps or worker.
+
+    :return: string
+                 "ps"
+                 "worker"
+    """
+    pod_resource_threshold = 0.95
+    is_scale_worker = False
+    for i in range(ps_number):
+        ps_cpu_mem_usage = pod_function.get_pod_cpu_memory_usage("gan-ps-{id}".format(id=i))
+        ps_cpu_mem_limit = pod_function.get_pod_cpu_memory_limits("gan-ps-{id}".format(id=i))
+        if ps_cpu_mem_usage[0]/ps_cpu_mem_limit[0] < pod_resource_threshold:
+            is_scale_worker = True
+            break
+    if is_scale_worker:
+        print("Scale worker!")
+        return "worker"
+    else:
+        print("Scale ps!")
+        return "ps"
+            
+
+def scale_ps_workerII(predict_training_time, job_submit_time, qos_time):
+    """Scale ps or worker smartly."""
+    scale_object = scale_ps_or_worker()
+    if scale_object == "ps":
+        delete_job()
+        scale_ps_horizontally()
+        submit_job()
+    else:
+        delete_job()
+        global worker_number
+        predict_scale_time = time.time()
+        # Reduce threshold
+        reduce_threshold = 1.2
+        scale_worker_number = math.ceil((reduce_threshold*worker_number*predict_training_time)/(job_submit_time+qos_time-predict_scale_time-scale_delay-load_data_delay))
+        change_worker_cmd = "sed -i 's/{{%- set worker_replicas = {number1} -%}}/{{%- set worker_replicas = {number2} -%}}/g' distributed-gan.jinja".format(
+                            number1=worker_number, number2=scale_worker_number)
+        worker_number = scale_worker_number
+        os.system(change_worker_cmd)
+        submit_job()
+
 
 
 def qos_guarantee(api_instance, qos_time):
@@ -70,9 +140,7 @@ def qos_guarantee(api_instance, qos_time):
         if forecast_complete_time <= job_submit_time + qos_time or worker_number >= max_worker_number:
             forecast_reach_qos = True
         else:
-            # Delete previous job
-            delete_job()
             # Scale the workers
-            scale_worker()
+            scale_ps_workerII((used_time/global_step/1.2)*total_steps, job_submit_time, qos_time)
     print("Scale Done! Wait Job Finish!")
     return pod_function.wait_job_finish(api_instance, namespace, pod_name, job_submit_time)
